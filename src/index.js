7dae1c1bada2bd9ee42ae0c451f5234f45f97037
Marcella Vizoná Vasconcelos <126676999+MarcellaVizona@users.noreply.github.com>
2024-06-12 11:58:12 -0300

    commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//Levanta O Servidor
+app.listen(process.env.PORT, function(){
+    console.log("Servidor executando na porta 3001");
+});
