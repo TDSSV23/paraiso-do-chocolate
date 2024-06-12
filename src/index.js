f3815dc57df0003c3ce30e5cb65fb6b3e92f1088
Moraes076 <felipe.desmoraes@gmail.com>
2024-06-12 16:36:07 -0300

    Update_conexao_bd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import {} from "dotenv/config.js";
 import express from "express";
 import cors from "cors";
-
+import { con } from "./config/database.js";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
 
 //Levanta O Servidor
 app.listen(process.env.PORT, function(){
