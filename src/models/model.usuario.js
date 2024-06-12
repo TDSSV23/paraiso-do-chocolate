cb53e95a99e0b460ab975dc2274e5aa469d8a33f
Marcella Vizoná Vasconcelos <126676999+MarcellaVizona@users.noreply.github.com>
2024-06-12 14:53:55 -0300

    commit

---
+++ src/models/model.usuario.js
@@ -0,0 +1,69 @@
+import bcrypt from "bcrypt";
+import {con} from "../config/databese.js";
+
+
+class   UsuarioModel {
+
+    // Método para obter todos os usuarios 
+        static getAllUsuarios (callback){
+            let sql = `select * from usuario`;
+
+            con.query(sql, function(err, result){
+                if(err)
+                    callback(err, null);
+                else
+                    callback(null, result);
+            });
+        }
+
+    // Método para criar um novo usuário
+
+    static createUsuario(nome, email, senha, callback){
+
+        //criptografar
+        const hash = bcrypt.hashSync(senha, 10);
+        senha = hash; 
+
+        let sql = `insert into usuario (nome, email, senha)
+        values(?, ?, ?)`;
+
+        con.query(sql, [nome, email, senha], function (err, result){
+            if (err)
+                callback(err, null);
+            else
+             callback(null, result);
+        });
+    }
+
+
+    //Método para editar um usuário existente
+
+    static editUsuario(id, nome, email, callback){
+        let sql = `update usuario set nome=? , email=? where id_usuario=?`;
+
+        con.query(sql, [nome, email, id], function (err, result) {
+            if(err)
+                callback(err, null);
+            else
+            callback(null, result);
+
+        });
+    }
+
+
+    // Método para remover um usuário 
+    static removeUsuario(id, callback){
+        let sql = `delete from usuario where id_usuario=?`;
+
+        con.query(sql, [id], function(err, result){
+            if(err)
+                callback(err, null)
+            else
+                callback(null, result)
+        });
+    }
+
+    
+}
+
+export default UsuarioModel;
---
+++ src/routes/route.usuario.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import UsuarioController from "../controllers/controller.usuario.js";
+
+const routeUsuario = Router();
+
+routeUsuario.get("/usuarios", UsuarioController.getAllUsuarios);
+
+
+export default routeUsuario;
